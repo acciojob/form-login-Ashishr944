0673cd41fbd1f05fed83d425c499082f0e01c693
Ashish Rokade <ashishrokade944@gmail.com>
2026-09-14 17:38:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getFormvalue(event) {
 
 	event.preventDefault();
 	const form = document.getElementById("form1");
-	const firstName = form.elements["fname"].values.trim();
+	const firstName = form.elements["fname"].value.trim();	
 	const lastName = form.elements["lname"].value.trim();
 	alert(firstName + " " + lastName);
 }
